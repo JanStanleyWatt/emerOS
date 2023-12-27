61d8670e8f046448045d6369dc520d1a895395b1
Jan Stanley Watt <12198769+JanStanleyWatt@users.noreply.github.com>
2023-12-28 02:45:00 +0900

    Add doc to module, structs, and methods in `color.rs`

--- kernel/src/graphic/color.rs
+++ kernel/src/graphic/color.rs
@@ -1,5 +1,10 @@
+//! bootloader_api::info::PixelFormatに準拠した色表現の内、`PixelFormat::Rgb`と`PixelFormat::Bgr`の部分を定義するモジュール
+//!
+//! ## Panic
+//! グレースケール(`bootloader_api::info::PixelFormat::U8`)のフレームバッファで`Color::encode(&self)`を呼び出すとパニックを起こす
 use bootloader_api::info::PixelFormat;
 
+/// bootloader_api::info::PixelFormatに準拠した色表現の内、`PixelFormat::Rgb`と`PixelFormat::Bgr`の部分を定義する
 #[derive(Copy, Clone, Debug, Hash, Default)]
 pub(crate) struct Color {
     pub(crate) red: u8,
@@ -8,18 +13,28 @@ pub(crate) struct Color {
 }
 
 impl Color {
+    /// 構造体`Color`の新規作成
     #[inline(always)]
     pub const fn new(red: u8, green: u8, blue: u8) -> Self {
         Color { red, green, blue }
     }
 
-    /// Color->配列
+    /// Color->配列への変換を行うメソッド
+    ///
+    /// ## Panic
+    /// グレースケール(`bootloader_api::info::PixelFormat::U8`)のフレームバッファでこのメソッドを呼び出すとパニックを起こす
+    ///
+    /// 2023年12月28日現在、グレースケールは未実装かつ実装すべきかどうかもまだわからないことから、`Color::encode(&self)`で
+    /// "The format is not supported by this struct"というメッセージと共にパニックした場合は真っ先に上記を疑う事
+    ///
+    /// 言うまでもなく、`bootloader_api::info::PixelFormat`に定義されていないフォーマットの場合でもパニックを起こす
     #[inline]
     pub const fn encode(&self, pixel_format: PixelFormat) -> [u8; 3] {
         match pixel_format {
             PixelFormat::Rgb => [self.red, self.green, self.blue],
             PixelFormat::Bgr => [self.blue, self.green, self.red],
-            PixelFormat::U8 => todo!(),
+            PixelFormat::U8 => panic!("The format is not supported by this struct"),
+            // Unknownなので決め打ち
             PixelFormat::Unknown {
                 red_position,
                 green_position,
