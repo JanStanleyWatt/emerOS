6ddb69ee1ae6623706e03236b8d98878d7d96868
Jan Stanley Watt <12198769+JanStanleyWatt@users.noreply.github.com>
2024-01-12 02:09:01 +0900

    Refactoring `graphic` module

--- kernel/src/graphic.rs
+++ kernel/src/graphic.rs
@@ -1,18 +1,32 @@
 //! グラフィック関連の機能を定義するモジュール
+//! 出力できる文字は [PlemolJP Console Nerds](https://github.com/yuru7/PlemolJP) に収録されたもののうち、
+//! TextとBoldの二種類の太さのうちどれか一つに限る
+//!
+//!
+//! ## LICENSE
+//! Copyright (c) 2021, Yuko OTAWARA. with Reserved Font Name "PlemolJP"
+//!
+//! This Font Software is licensed under the SIL Open Font License, Version 1.1.
+//! This license is copied below, and is also available with a FAQ at:
+//! https://scripts.sil.org/OFL
 
+pub(crate) mod color;
 pub(crate) mod console;
 pub(crate) mod text_buffer;
+
+use core::num::NonZeroUsize;
 
 use ab_glyph::FontRef;
 use alloc::boxed::Box;
 use bootloader_api::info::FrameBuffer;
 use common_lib::locked::Locked;
 
-use self::text_buffer::{TextBuffer, TextBufferInfo};
-use crate::{FRAME_BUFFER, FRAME_BUFFER_INFO, TEXT_BUFFER, TEXT_BUFFER_INFO};
+use self::text_buffer::TextBuffer;
+use crate::{FRAME_BUFFER, FRAME_BUFFER_INFO, TEXT_BUFFER, TEXT_BUFFER_HEIGHT, TEXT_BUFFER_WIDTH};
 
 const FONT_TEXT: &[u8; 4259456] = include_bytes!("graphic/resources/PlemolJPConsoleNF-Text.ttf");
 const FONT_BOLD: &[u8; 4257220] = include_bytes!("graphic/resources/PlemolJPConsoleNF-Bold.ttf");
+const FONT_SCALE: f32 = 24.0;
 
 /// 描画モジュールの初期化
 pub(crate) fn init(frame_buffer: &'static mut FrameBuffer) {
@@ -28,17 +42,16 @@ pub(crate) fn init(frame_buffer: &'static mut FrameBuffer) {
         let font_text = FontRef::try_from_slice(FONT_TEXT).expect("Failed to load text font data");
         let font_bold = FontRef::try_from_slice(FONT_BOLD).expect("Failed to load bold font data");
         Box::new(Locked::new(TextBuffer::new(
-            font_text,
-            font_bold,
-            24.0,
-            FRAME_BUFFER_INFO.get().unwrap(),
+            font_text, font_bold, FONT_SCALE,
         )))
     });
 
-    TEXT_BUFFER_INFO.get_or_init(|| {
-        let info = FRAME_BUFFER_INFO.get().unwrap();
-        let scale = TEXT_BUFFER.get().unwrap().lock().scale;
+    let info = FRAME_BUFFER_INFO.get().unwrap();
 
-        Box::new(TextBufferInfo::new(info.stride, info.height, scale))
-    });
+    let width = info.stride / (FONT_SCALE / 2.0) as usize;
+    let height = info.height / FONT_SCALE as usize;
+
+    // WIDTHもHEIGHTも割り算で求めるため、`info.stride`や`info.stride`が0でない限り（まずありえない）0を渡すことは無い
+    TEXT_BUFFER_WIDTH.get_or_init(move || unsafe { NonZeroUsize::new_unchecked(width) });
+    TEXT_BUFFER_HEIGHT.get_or_init(move || unsafe { NonZeroUsize::new_unchecked(height) });
 }
